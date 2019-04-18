6c470e98587491090ea7ea4cc2efd49e6ef43093
arthur <arthur.teisseire@epitech.eu>
2019-04-18 11:44:19 +0200

    Accept multiple command in CommandParser regex

--- src/parse/CommandParser.cpp
+++ src/parse/CommandParser.cpp
@@ -11,14 +11,14 @@
 
 plazza::CommandParser::CommandParser(const std::string &commandLine)
 {
-    std::regex e("^([a-zA-Z]+) (S|M|L|XL|XXL) x([1-9][0-9]*)$");
+    std::regex e("^(([a-zA-Z]+) (S|M|L|XL|XXL) x([1-9][0-9]*))+$");
     std::smatch m;
     std::string res;
 
     if (std::regex_search(commandLine, m, e)) {
-        command.type = m[1];
-        command.size = m[2];
-        command.number = std::stoi(m[3]);
+        command.type = m[2];
+        command.size = m[3];
+        command.number = std::stoi(m[4]);
     } else {
         std::cerr << "not a valid command" << std::endl;
     }
