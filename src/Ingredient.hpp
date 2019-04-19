51cadc2c80a7a40420f4a0892d1245e175ca7987
arthur <arthur.teisseire@epitech.eu>
2019-04-19 11:13:58 +0200

    Add Ingredient enum

---
+++ src/Ingredient.hpp
@@ -0,0 +1,24 @@
+/*
+** EPITECH PROJECT, 2018
+** plazza
+** File description:
+** Ingredients.hpp
+*/
+
+#ifndef PLAZZA_INGREDIENT_HPP
+#define PLAZZA_INGREDIENT_HPP
+
+namespace plazza {
+    enum Ingredient {
+        DOE,
+        TOMATOE,
+        GRUYERE,
+        HAM,
+        STEAK,
+        EGGPLANT,
+        GOAT_CHEESE,
+        CHIEF_LOVE
+    };
+}
+
+#endif
